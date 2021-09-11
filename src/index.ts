463c5a6e1030625a314c17e4017605b3f112b6a7
vinodbavisetti <68192836+vinodbavisetti@users.noreply.github.com>
2021-09-12 01:48:15 +0530

    committin in git hub page after uploading directly

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { Subject } from "rxjs";
+import { func } from "./subfile";
+import "./index.scss";
+
+let subject = new Subject();
+
+subject.subscribe(func);
+
+document.querySelector("button").addEventListener("click", (e) => {
+  let p = document.querySelector("p");
+  let y = p.offsetTop;
+  // document.documentElement.scrollBy({ left: 0, top: y, behavior: "smooth" });
+  // p.scrollBy({ left: 0, top: y, behavior: "smooth" });
+  subject.next(p);
+});
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+      },
+      // {
+      //   test: /\.scss/,
+      //   loader: ["style-loader", "css-loader", "sass-loader"],
+      // },
+      {
+        test: /\.scss/,
+        loader: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html", filename: "index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: "source-map",
+};
